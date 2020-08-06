410a43d1487f0daa78af83b82f1da30089da2322
vaibhav walekar <vaibhavwalekar2013@gmail.com>
2020-08-06 21:34:53 +0530

    Auth Code

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -17,7 +17,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true,
+    secure: true
   })
 );
 
@@ -33,11 +33,15 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
+      useCreateIndex: true
     });
     console.log('Connected to MongoDb');
   } catch (err) {
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,28 +1,26 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
   '/api/users/signup',
   [
-    body('email').isEmail().withMessage('Email must be valid'),
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
     body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters'),
+      .withMessage('Password must be between 4 and 20 characters')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -33,18 +31,19 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
-    // generate jWT
-    const userJWT = jwt.sign(
+
+    // Generate JWT
+    const userJwt = jwt.sign(
       {
         id: user.id,
-        email: user.email,
+        email: user.email
       },
-      'asdf'
+      process.env.JWT_KEY!
     );
 
-    // store it on session.
+    // Store it on session object
     req.session = {
-      jwt: userJWT,
+      jwt: userJwt
     };
 
     res.status(201).send(user);
