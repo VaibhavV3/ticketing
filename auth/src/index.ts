bff2fd26e1abc9f30ca5ed2fbcf30164ff70c7be
vaibhav walekar <vaibhavwalekar2013@gmail.com>
2020-08-08 21:13:19 +0530

    Common Code creation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,8 +8,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { errorHandler, NotFoundError } from '@v3test/common';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,7 +16,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true
+    secure: true,
   })
 );
 
@@ -41,7 +40,7 @@ const start = async () => {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
     console.log('Connected to MongoDb');
   } catch (err) {
